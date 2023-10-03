732415ef3031b888823a5b36a888aa534893f5b4
Franklin Cappa Ticona <desarrollador220_usi@minedu.gob.pe>
2023-10-03 10:52:05 -0500

    Implementando servicios

--- frontEnd/src/app/components/contacto/contacto.component.ts
+++ frontEnd/src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { contactoModel } from 'src/app/model/contacto-model';
+import { ContactoService } from 'src/app/service/contacto.service';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +9,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.sass']
 })
 export class ContactoComponent {
+  listaContactos: contactoModel[]=[];
+  formContacto: FormGroup=new FormGroup({});
+  isUpdate: Boolean = false;
+
+  constructor(private contactoService: ContactoService){}
+  ngOnInit():void{
+    this.listar();
+    this.formContacto= new FormGroup({
+      id:new FormControl(''),
+      nombres: new FormControl(''),
+      apellidos: new FormControl(''),
+      celular: new FormControl(''),
+      email: new FormControl(''),
+      direccion: new FormControl('')
+    })
+  }
+
+  listar(){
+    this.contactoService.getContactos().subscribe(resp=>{
+      if(resp){
+        this.listaContactos=resp;
+      }
+    })
+  }
+
+  nuevoContacto(){
+    this.isUpdate=false;
+    this.formContacto.reset();
+  }
+
+  selectItem(item: any){
+    this.isUpdate=true;
+    this.formContacto.controls['id'].setValue(item.id);
+    this.formContacto.controls['nombres'].setValue(item.nombres);
+    this.formContacto.controls['apellidos'].setValue(item.apellidos);
+    this.formContacto.controls['celular'].setValue(item.celular);
+    this.formContacto.controls['email'].setValue(item.email);
+    this.formContacto.controls['direccion'].setValue(item.direccion);
+  }
+
+  save(){
+    this.formContacto.controls['estado'].setValue('1');
+    this.contactoService.saveContacto(this.formContacto.value).subscribe(resp=>{
+      if(resp){
+        this.listar();
+        this.formContacto.reset();
+      }  
+    })
+  }
+
+  update(){
+    this.contactoService.updateContacto(this.formContacto.value).subscribe(resp=>{
+      if(resp){
+        this.listar();
+        this.formContacto.reset();
+      }  
+    })
+  }
+
+  delete(id: any){
+    this.contactoService.deleteContacto(id).subscribe(resp=>{
+      if(resp){
+        this.listar();
+        this.formContacto.reset();
+      }  
+    })
+  }
 
 }
--- frontEnd/src/app/service/contacto.service.ts
+++ frontEnd/src/app/service/contacto.service.ts
@@ -1,17 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { contactoModel } from '../model/contacto-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactoService {
+  url = 'http://localhost:8090/api/v1/contacto'
 
   constructor(private httpClient:HttpClient ) {     
   }
 
-  getContactos: Observable<contactoModel[]>{
-    return 0; 
+  getContactos(): Observable<contactoModel[]>{
+    return this.httpClient.get<contactoModel[]>(this.url+'/list').pipe(map(res=> res ));
   }
+
+  saveContacto(request: any):Observable<any>{
+    return this.httpClient.post<any>(this.url+'/save', request).pipe(map(res=>res));
+  } 
+
+  updateContacto(request: any):Observable<any> {
+    return this.httpClient.post<any>(this.url+'/update', request).pipe(map(res=> res));
+  }
+
+  deleteContacto(id: number):Observable<any>{
+    return this.httpClient.get<any>(this.url+'/delete/'+id).pipe(map(res=>res));
+  } 
 }
